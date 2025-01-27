b766dfbd47f688e31c828aa55d1bf054f46f4c3a
aruneshwar16 <aruneshwar.em2023it@sece.ac.in>
2025-01-27 10:49:23 +0530

    new backend

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 // MongoDB Connection
 console.log('Connecting to MongoDB...');
-mongoose.connect('mongodb://127.0.0.1:27017/expense')
+mongoose.connect('mongodb://localhost:27017/expense')
 .then(() => console.log('MongoDB Connected Successfully!'))
 .catch(err => console.error('MongoDB Connection Error:', err));
 
